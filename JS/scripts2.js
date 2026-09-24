13f10c29336db8bf73d23f1e11769e0b2939ba64
Devoak <pedrojoaquimpereira8@gmail.com>
2026-09-24 00:07:48 -0300

    Começando a 4° questão

---
+++ JS/scripts2.js
@@ -0,0 +1,262 @@
+const alunos = {
+
+    ingles: {
+
+        A: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        B: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        C: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ]
+    },
+
+    espanhol: {
+
+        A: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        B: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        C: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ]
+    },
+
+    japones: {
+
+        A: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        B: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        C: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ]
+    },
+
+    frances: {
+
+        A: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        B: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ],
+
+        C: [
+            { nome: "Ana Silva", notas: [8.5, 7.5, 9.0, 8.0] },
+            { nome: "Bruno Santos", notas: [7.0, 8.0, 7.5, 9.0] },
+            { nome: "Carlos Oliveira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Daniel Souza", notas: [7.5, 8.0, 8.5, 7.0] },
+            { nome: "Eduarda Lima", notas: [9.0, 9.5, 8.0, 9.0] },
+            { nome: "Felipe Costa", notas: [6.5, 7.0, 8.0, 7.5] },
+            { nome: "Gabriel Alves", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "João Pereira", notas: [9.0, 8.5, 9.5, 9.0] },
+            { nome: "Maria Souza", notas: [8.0, 8.5, 9.0, 8.5] },
+            { nome: "Helena Martins", notas: [9.0, 9.0, 8.5, 9.5] }
+        ]
+    }
+
+};
+
+const alunosIngles = [
+    ...alunos.ingles.A,
+    ...alunos.ingles.B,
+    ...alunos.ingles.C
+];
+
+const alunosEspanhol = [
+    ...alunos.espanhol.A,
+    ...alunos.espanhol.B,
+    ...alunos.espanhol.C
+];
+
+const alunosJapones = [
+    ...alunos.japones.A,
+    ...alunos.japones.B,
+    ...alunos.japones.C
+];
+
+const alunosFrances = [
+    ...alunos.frances.A,
+    ...alunos.frances.B,
+    ...alunos.frances.C
+];
+
+function calcularMedia(notas) {
+
+    let soma = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+
+        soma = soma + notas[i];
+
+    }
+
+    return soma / notas.length;
+
+}
+
+
+function adicionarMedias(alunos) {
+
+    for (let i = 0; i < alunos.length; i++) {
+
+        alunos[i].media = calcularMedia(alunos[i].notas);
+
+    }
+
+}
+
+
+function criarPodio(alunos) {
+
+    let disponiveis = [...alunos];
+
+    const podio = [];
+
+    for (let posicao = 0; posicao < 3; posicao++) {
+
+        let melhor = 0;
+
+        for (let i = 1; i < disponiveis.length; i++) {
+
+            if (disponiveis[i].media > disponiveis[melhor].media) {
+
+                melhor = i;
+
+            }
+
+        }
+
+        podio.push(disponiveis[melhor]);
+
+        disponiveis.splice(melhor, 1);
+
+    }
+
+    return podio;
+
+}
+
+adicionarMedias(alunosIngles);
+adicionarMedias(alunosEspanhol);
+adicionarMedias(alunosJapones);
+adicionarMedias(alunosFrances);
+
+
+const podioIngles = criarPodio(alunosIngles);
+const podioEspanhol = criarPodio(alunosEspanhol);
+const podioJapones = criarPodio(alunosJapones);
+const podioFrances = criarPodio(alunosFrances);
